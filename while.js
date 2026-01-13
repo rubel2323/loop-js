b58ad24fd0036fd62f63831f7b9b353f9d4ceae5
Rubel <mr.adcadre41@gmail.com>
2026-01-13 22:46:52 +0600

    while conditions added

--- while.js
+++ while.js
@@ -1,6 +1,10 @@
-const num = 5;
-if (num < 11) {
-  console.log("Number is greater than 10");
-} else {
-  console.log("Number is 10 or less");
+let num = 0;
+// if (num < 11) {
+//   console.log("Number is greater than 10");
+// } else {
+//   console.log("Number is 10 or less");
+// }
+while (num < 11) {
+  console.log("Number is greater than", num);
+  num++;
 }
