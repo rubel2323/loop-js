cdd0e4cb74e3bcc84da2ae420cf52f911e3ec0a8
Rubel <mr.adcadre41@gmail.com>
2026-01-31 22:22:26 +0600

    while-2 added

---
+++ while2.js
@@ -0,0 +1,8 @@
+let message = 0;
+while (message <= 60) {
+  console.log(
+    "I will invest at least 6 hrs every single day for next 60 days! This is day number: ",
+    message,
+  );
+  message++;
+}
