b3e3b8b266c9ef3dded7d484449af024a3299719
Rubel <mr.adcadre41@gmail.com>
2026-01-31 19:20:25 +0600

    interval added

---
+++ interval.js
@@ -0,0 +1,13 @@
+// for (let i = 81; i >= 65; i--) {
+//   console.log(i);
+// }
+
+let count = 81;
+const timer = setInterval(() => {
+  console.log(count);
+  count--;
+
+  if (count < 65) {
+    clearInterval(timer);
+  }
+}, 10);
---
+++ multiplication.js
@@ -0,0 +1,5 @@
+const num = 5;
+for (let i = 1; i <= 15; i++) {
+  const result = num * i;
+  console.log(`${num} * ${i} = ${result}`);
+}
