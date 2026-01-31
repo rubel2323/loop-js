c527526a13cb26513e4dfe8b304589e5a7797f15
Rubel <mr.adcadre41@gmail.com>
2026-01-31 22:30:08 +0600

    while_odd_even.js

---
+++ while_odd_even.js
@@ -0,0 +1,14 @@
+let num = 0;
+while (num <= 100) {
+  if (num % 2 === 0) {
+    console.log(num + " is even number");
+  }
+  num++;
+}
+let num2 = 78;
+while (num2 <= 98) {
+  if (num2 % 2 !== 0) {
+    console.log(num2 + " is odd number");
+  }
+  num2++;
+}
